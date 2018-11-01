90ba12c9af3e5921c62c2dbf378793fe4600c4a3
user10112018 <raspberry.gooseberry@gmail.com>
2018-11-01 17:56:07 +0900

    until 1101

--- ecsite/src/com/internousdev/ecsite/dao/LoginDAO.java
+++ ecsite/src/com/internousdev/ecsite/dao/LoginDAO.java
@@ -3,6 +3,9 @@ package com.internousdev.ecsite.dao;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import com.internousdev.ecsite.dto.LoginDTO;
 import com.internousdev.ecsite.util.DBConnector;
@@ -12,6 +15,9 @@ public class LoginDAO {
 	private DBConnector dbConnector = new DBConnector();
 	private Connection connection = dbConnector.getConnection();
 	private LoginDTO loginDTO = new LoginDTO();
+
+//	1101add
+	List<LoginDTO> loginDTOList = new ArrayList<LoginDTO>();
 
 	public LoginDTO getLoginUserInfo(String loginUserId, String loginPassword){
 
@@ -32,6 +38,9 @@ public class LoginDAO {
 				loginDTO.setLoginPassword(resultSet.getString("login_pass"));
 				loginDTO.setUserName(resultSet.getString("user_name"));
 
+//				1101add
+//				loginDTOList.add(loginDTO);
+
 				if(!(resultSet.getString("login_id").equals(null))){
 					loginDTO.setLoginFlg(true);
 				}
@@ -47,5 +56,39 @@ public class LoginDAO {
 	public LoginDTO getLoginDTO(){
 		return loginDTO;
 	}
+
+//	1101add ======================================================
+//     DBを読んでユーザー情報をリストにまとめるためだけのメソッド
+	public List<LoginDTO> select(){
+
+		DBConnector db = new DBConnector();
+		Connection con = db.getConnection();
+
+		String sql = "SELECT * FROM login_user_transaction";
+
+		try {
+
+			PreparedStatement ps = con.prepareStatement(sql);
+			ResultSet rs = ps.executeQuery();
+
+			if(rs.next()){
+				loginDTO.setLoginId(rs.getString("login_id"));
+				loginDTO.setLoginPassword(rs.getString("login_pass"));
+				loginDTO.setUserName(rs.getString("user_name"));
+
+				loginDTOList.add(loginDTO);
+			}
+
+	    } catch (SQLException e){
+		e.printStackTrace();
+	    }
+	     try {
+		      con.close();
+	     } catch (SQLException e){
+		e.printStackTrace();
+	    }
+	return loginDTOList;
+	}
+//	1101add ======================================================
 
 }
